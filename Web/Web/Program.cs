929d3348ec0c16979fe9050c390da35ff1540855
intotf <intotf@163.com>
2018-06-25 22:41:22 +0800

    修改为 IIS Express

--- Web/Web/Program.cs
+++ Web/Web/Program.cs
@@ -17,21 +17,16 @@ namespace Web
             CreateWebHostBuilder(args).Build().Run();
         }
 
-        //public static IWebHost BuildWebHost(string[] args) =>
-        //                    WebHost.CreateDefaultBuilder(args)
-        //                    .UseUrls("http://localhost:7002;http://*:7003")		//指定端口
-        //                    .UseKestrel()   //应用为服务器使用的web主机。
-        //                    .UseContentRoot(Directory.GetCurrentDirectory())
-        //                    .UseIISIntegration()
-        //                    .UseStartup<Startup>()
-        //                    .Build();
-
         public static IWebHostBuilder CreateWebHostBuilder(string[] args) =>
-            WebHost.CreateDefaultBuilder(args)
-                .UseUrls("http://localhost:7002;http://*:7003")		//指定端口
-                .UseKestrel()   //应用为服务器使用的web主机。
-                .UseContentRoot(Directory.GetCurrentDirectory())
-                .UseIISIntegration()
-                .UseStartup<Startup>();
+           WebHost.CreateDefaultBuilder(args)
+               .UseStartup<Startup>();
+
+        //public static IWebHostBuilder CreateWebHostBuilder(string[] args) =>
+        //    WebHost.CreateDefaultBuilder(args)
+        //        .UseUrls("http://localhost:7002;http://*:7003")		//指定端口
+        //        .UseKestrel()   //应用为服务器使用的web主机。
+        //        .UseContentRoot(Directory.GetCurrentDirectory())
+        //        .UseIISIntegration()
+        //        .UseStartup<Startup>();
     }
 }
